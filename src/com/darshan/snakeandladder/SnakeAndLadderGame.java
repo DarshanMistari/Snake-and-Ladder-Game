8e4641fe000fbc2db8464aef3b3c143b080e0851
DarshanMistari <mistaridarshan2@gmail.com>
2024-03-09 19:47:57 +0530

    [MODIFIED]:Darshan -UC-2 Player Start Game in Roll Die 1 to 6 Number

--- src/com/darshan/snakeandladder/SnakeAndLadderGame.java
+++ src/com/darshan/snakeandladder/SnakeAndLadderGame.java
@@ -1,5 +1,7 @@
 package com.darshan.snakeandladder;
 
+import java.util.Random;
+
 public class SnakeAndLadderGame {
 	
 	private int playerPosition = 0;
@@ -8,13 +10,22 @@ public class SnakeAndLadderGame {
 	{
 		System.out.println("Player Position :" + playerPosition);
 	}
-
+	
+	public void playGame()
+	{
+		Random random = new Random(); 
+		
+		int dieNo = random.nextInt(6)+1;
+		System.out.println("Die No :" +dieNo);
+	}
+	
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Snake and Ladder Game Developed by Darshan Mistari");
 		
 		SnakeAndLadderGame snakeandladdergame = new SnakeAndLadderGame();
 		snakeandladdergame.showPosition();
+		snakeandladdergame.playGame();
 	}
 
 }
