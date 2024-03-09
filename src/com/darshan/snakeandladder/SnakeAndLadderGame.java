37b94235fa21404abbfc6156f3a34f4f6f4b9dd6
DarshanMistari <mistaridarshan2@gmail.com>
2024-03-09 19:25:04 +0530

    [MoDIFIED]:Darshan - UC-1 Game is Start Player Position Zero

--- src/com/darshan/snakeandladder/SnakeAndLadderGame.java
+++ src/com/darshan/snakeandladder/SnakeAndLadderGame.java
@@ -1,10 +1,20 @@
 package com.darshan.snakeandladder;
 
 public class SnakeAndLadderGame {
+	
+	private int playerPosition = 0;
+	
+	public void showPosition()
+	{
+		System.out.println("Player Position :" + playerPosition);
+	}
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Snake and Ladder Game Developed by Darshan Mistari");
+		
+		SnakeAndLadderGame snakeandladdergame = new SnakeAndLadderGame();
+		snakeandladdergame.showPosition();
 	}
 
 }
